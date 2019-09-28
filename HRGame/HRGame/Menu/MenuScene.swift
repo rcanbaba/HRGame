668ebf374f24fbda70f4f441750cf2d4009d438b
Can Babaoğlu <can.babaoglu@rempeople.com>
2019-09-28 17:26:43 +0300

    - MenuScene oluşturuldu. - Menu de stageLabel eklendi stage durumu userDefault a kaydediliyor.

---
+++ HRGame/HRGame/Menu/MenuScene.swift
@@ -0,0 +1,92 @@
+//
+//  MenuScene.swift
+//  HRGame
+//
+//  Created by Can Babaoğlu on 28.09.2019.
+//  Copyright © 2019 Can Babaoğlu. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+
+    var starfield:SKEmitterNode!
+    
+    var newGameButtonNode:SKSpriteNode!
+    var stageButtonNode:SKSpriteNode!
+    var stageLabelNode:SKLabelNode!
+    
+    override func didMove(to view: SKView) {
+        starfield = (self.childNode(withName: "starfield") as! SKEmitterNode)
+        starfield.advanceSimulationTime(10)
+        
+        newGameButtonNode = (self.childNode(withName: "newGameButton") as! SKSpriteNode)
+        stageButtonNode = (self.childNode(withName: "stageButton") as! SKSpriteNode)
+        stageButtonNode.texture = SKTexture(imageNamed: "stageButton")
+        
+        stageLabelNode = (self.childNode(withName: "stageLabel") as! SKLabelNode)
+        
+        let userDefaults = UserDefaults.standard
+        
+        if(userDefaults.integer(forKey: "stageKey") == 0 ){
+            stageLabelNode.text = "Tutorial"
+        }else if(userDefaults.integer(forKey: "stageKey") == 1){
+            stageLabelNode.text = "Stage 1"
+        }else if(userDefaults.integer(forKey: "stageKey") == 2){
+            stageLabelNode.text = "Stage 2"
+        }else if(userDefaults.integer(forKey: "stageKey") == 3){
+            stageLabelNode.text = "Stage 3"
+        }else{
+            
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            let nodesArray = self.nodes(at: location)
+            if (nodesArray.first?.name == "newGameButton"){
+                
+                //let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+               // let gameScene = GameScene(size: self.size)
+                //self.view?.presentScene(gameScene,transition: transition)
+                
+                if let scene = SKScene(fileNamed: "GameScene") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    // Present the scene
+                    let transition = SKTransition.doorsOpenHorizontal(withDuration: 1.5)
+                    self.view!.presentScene(scene, transition: transition)
+                }
+            }else if(nodesArray.first?.name == "stageButton"){
+                changeStage()
+                
+            }
+        }
+    }
+    
+    func changeStage(){
+        let userDefaults = UserDefaults.standard
+        
+        if (stageLabelNode.text == "Tutorial"){
+            stageLabelNode.text = "Stage 1"
+            userDefaults.set(1 , forKey: "stageKey")
+        }else if(stageLabelNode.text == "Stage 1"){
+            stageLabelNode.text = "Stage 2"
+            userDefaults.set(2 , forKey: "stageKey")
+        }else if(stageLabelNode.text == "Stage 2"){
+            stageLabelNode.text = "Stage 3"
+            userDefaults.set(3 , forKey: "stageKey")
+        }else if(stageLabelNode.text == "Stage 3"){
+            stageLabelNode.text = "Tutorial"
+            userDefaults.set(0 , forKey: "stageKey")
+        }else{
+            
+        }
+        userDefaults.synchronize()
+    }
+    
+    
+}
